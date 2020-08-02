87ea4b266a83517b099037dea5a80f5c828cccd1
adamHamland <hamiltad@oregonstate.edu>
2020-08-02 00:36:39 -0700

    Improved scripts code base readabilty and the messages produced while running have been chnaged to be more informative. Script seems to consitently exit when captcha is found but more tests need to be run.

--- pokebotAutoScript.py
+++ pokebotAutoScript.py
@@ -37,21 +37,21 @@ def gameLoop():
 
     if poke == '':
         exit()
-    
-    delay = random.randrange(2,4)
-    time.sleep(delay)
+    else:
+        delay = random.randrange(2,4)
+        time.sleep(delay)
 
-    keyboard.type(poke)
+        keyboard.type(poke)
 
-    time.sleep(1)
+        time.sleep(1)
 
-    keyboard.press(Key.enter)
-    keyboard.release(Key.enter)
+        keyboard.press(Key.enter)
+        keyboard.release(Key.enter)
 
 
 def copyMsg():
     a,b = mouse.position
-    mouse.position=(a,920)
+    mouse.position=(a,890)
 
     time.sleep(.3)
     
@@ -71,22 +71,26 @@ def copyMsg():
 def readClipboard():
     text = clipboard.paste()
     
-    print(text)
-
     if 'captcha' in text:
+        print("Captcha detected. Exiting...")
         return ''
     elif 'Shiny' in text:
+        print("Shiny found. Throwing mb.", "\n\n---\n")
         return 'mb'
     elif 'Legendary' in text:
+        print("Legendary found. Throwing ub.", "\n\n---\n")
         return 'ub'
     elif 'Super Rare' in text:
+        print("Super Rare found. Throwing gb.", "\n\n---\n")
         return 'gb'
     else:
+        print("Common, Uncommon, or Rare found. Throwing pb.", "\n\n---\n")
         return 'pb'
 
 #main
 
-for x in range(0,1):
+for x in range(0,10): #no way this runs more than 10 times without getting the captcha
+
     time.sleep(10)
     
     keyboard.type(';shop buy 1 20')
@@ -96,7 +100,7 @@ for x in range(0,1):
     keyboard.press(Key.enter)
     keyboard.release(Key.enter)
 
-    for x in range(0,5):
+    for x in range(0,20):
         gameLoop()
     
 
